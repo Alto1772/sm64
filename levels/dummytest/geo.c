cf3254cf66d19122ce3581a34097d4b5ac2548fc
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2020-10-01 13:19:37 +0800

    added 37th stage

---
+++ levels/dummytest/geo.c
@@ -0,0 +1,21 @@
+/*********************************************************************************
+ *            Geometry Include Data for dummytest (aka dummy test map)             *
+ *                                                                               *
+ *********************************************************************************/
+
+#include <ultra64.h>
+#include "sm64.h"
+#include "geo_commands.h"
+
+#include "game/level_geo.h"
+#include "game/geo_misc.h"
+#include "game/camera.h"
+#include "game/moving_texture.h"
+#include "game/screen_transition.h"
+#include "game/paintings.h"
+
+#include "make_const_nonconst.h"
+
+#include "levels/dummytest/header.h"
+
+#include "levels/dummytest/areas/1/geo.inc.c"
---
+++ levels/dummytest/header.h
@@ -0,0 +1,16 @@
+#ifndef DUMMYTEST_HEADER_H
+#define DUMMYTEST_HEADER_H
+
+#include "types.h"
+
+// geo
+extern const GeoLayout dummytest_geo[];
+
+// leveldata
+extern const Collision dummytest_seg7_collision[];
+extern const Gfx dummytest_seg7_dl[];
+
+// script
+extern const LevelScript level_dummytest_entry[];
+
+#endif
