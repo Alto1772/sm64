530b319284d5d8060e21682afa3aafe211c193e4
Alto1772 <56553686+Alto1772@users.noreply.github.com>
2020-10-08 17:13:41 +0800

    changed dummytest to dummy

---
+++ levels/dummy/header.h
@@ -0,0 +1,16 @@
+#ifndef DUMMYTEST_HEADER_H
+#define DUMMYTEST_HEADER_H
+
+#include "types.h"
+
+// geo
+extern const GeoLayout dummy_geo[];
+
+// leveldata
+extern const Collision dummy_seg7_collision[];
+extern const Gfx dummy_seg7_dl[];
+
+// script
+extern const LevelScript level_dummy_entry[];
+
+#endif
